37bbab1d3846373213d1db1df0d6491e84e4dc56
Aneesh <aneesh1701@gmail.com>
2023-11-14 15:43:23 +0530

    add js solution

---
+++ main.js
@@ -0,0 +1,14 @@
+// run using NodeJS
+
+const fs = require("fs");
+
+const readData = fs.readFileSync("./input.txt", "utf-8"); // Read input file
+
+const squares = readData
+	.split(/\r?\n/) // Split into string[]
+	.map((str) => Number(str.trim())) // Convert to number[]
+	.map((num) => num * num); // Square each number
+
+const sum = squares.reduce((previous, current) => previous + current, 0); // Find sum of each squared number
+
+console.log(`The value is ${sum}`);
